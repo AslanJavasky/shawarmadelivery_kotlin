d4e621216ea00a41e9ee19b8fce8a564361ab3c1
Aslan Javasky <71359713+AslanJavasky@users.noreply.github.com>
2025-01-22 23:04:28 +0300

    8 @Lazy

--- src/main/kotlin/com/aslanjavasky/shawarmadelviry/presentation/controller/UserController.kt
+++ src/main/kotlin/com/aslanjavasky/shawarmadelviry/presentation/controller/UserController.kt
@@ -2,12 +2,20 @@ package com.aslanjavasky.shawarmadelviry.presentation.controller
 
 import com.aslanjavasky.shawarmadelviry.domain.model.User
 import com.aslanjavasky.shawarmadelviry.presentation.service.UserService
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.Lazy
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Controller
 
 @Controller
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
 class UserController(
     private val userService: UserService
 ) {
+    init {
+        println("UserController bean is created!")
+    }
     fun createUser(user: User) = userService.createUser(user)
 
     fun deleteUser(user: User) = userService.deleteUser(user)
