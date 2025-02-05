b05514e5cd132e1e5dd82e7407d6b91e5fea39f7
Aslan Javasky <71359713+AslanJavasky@users.noreply.github.com>
2025-02-05 16:53:07 +0300

    35 connection transactions: commit||rollback

--- src/main/kotlin/com/aslanjavasky/shawarmadelviry/data/repoImpls/statement/OrderRepoImpl.kt
+++ src/main/kotlin/com/aslanjavasky/shawarmadelviry/data/repoImpls/statement/OrderRepoImpl.kt
@@ -5,6 +5,7 @@ import com.aslanjavasky.shawarmadelviry.domain.repo.MenuItemRepo
 import com.aslanjavasky.shawarmadelviry.domain.repo.OrderRepo
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.sql.SQLException
 import java.sql.Statement
 import java.sql.Timestamp
@@ -21,34 +22,41 @@ class OrderRepoImpl(
         val sqlOrder = "INSERT INTO orders(date_time, status, user_id, total_price) VALUES(?,?,?,?);"
         val sqlOrderMenuitems = "INSERT INTO orders_menu_items(order_id, menu_item_id) VALUES(?,?);"
         dataSource.connection.use { connection ->
-            connection.prepareStatement(sqlOrder, Statement.RETURN_GENERATED_KEYS).use { ps ->
-                ps.setTimestamp(1, Timestamp.valueOf(order.dateTime))
-                ps.setString(2, order.status!!.name)
-                ps.setLong(3, order.user!!.id!!)
-                ps.setBigDecimal(4, order.totalPrice)
+            connection.autoCommit = false
+            try {
+                connection.prepareStatement(sqlOrder, Statement.RETURN_GENERATED_KEYS).use { ps ->
+                    ps.setTimestamp(1, Timestamp.valueOf(order.dateTime))
+                    ps.setString(2, order.status!!.name)
+                    ps.setLong(3, order.user!!.id!!)
+                    ps.setBigDecimal(4, order.totalPrice)
 
-                val affectedRow = ps.executeUpdate()
-                if (affectedRow == 0) throw SQLException("Failed to save order, no rows affected")
+                    val affectedRow = ps.executeUpdate()
+                    if (affectedRow == 0) throw SQLException("Failed to save order, no rows affected")
 
-                ps.generatedKeys.use { rs ->
-                    while (rs.next()) {
-                        order.id = rs.getLong("id")
+                    ps.generatedKeys.use { rs ->
+                        while (rs.next()) {
+                            order.id = rs.getLong("id")
+                        }
+                    }
+
+                }
+                connection.prepareStatement(sqlOrderMenuitems).use { ps ->
+                    for (item in order.itemList!!) {
+                        ps.setLong(1, order.id!!)
+                        ps.setLong(2, item!!.id)
+                        ps.addBatch()
+                    }
+                    val batchResults = ps.executeBatch()
+                    for (result in batchResults) {
+                        if (result == Statement.EXECUTE_FAILED) {
+                            throw SQLException("Failed to save menuitem related to order, no rows affected")
+                        }
                     }
                 }
-
-            }
-            connection.prepareStatement(sqlOrderMenuitems).use { ps ->
-                for (item in order.itemList!!) {
-                    ps.setLong(1, order.id!!)
-                    ps.setLong(2, item!!.id)
-                    ps.addBatch()
-                }
-                val batchResults = ps.executeBatch()
-                for (result in batchResults) {
-                    if (result == Statement.EXECUTE_FAILED) {
-                        throw SQLException("Failed to save menuitem related to order, no rows affected")
-                    }
-                }
+                connection.commit()
+            }catch (e:SQLException){
+                connection.rollback()
+                e.printStackTrace()
             }
         }
         return order
